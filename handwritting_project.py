ab7408f4b2008fe17a612770a963d16be06879fc
ABIDUL HASAN AMIN <113846592+Amin-Hossain@users.noreply.github.com>
2025-03-03 23:28:32 +0600

    Add files via upload
    First You need to copy pywhatkit . Then you go to the terminal of vscode or pychar and then paste or you can write there "pip install pywhatkit

---
+++ handwritting_project.py
@@ -0,0 +1,6 @@
+ import pywhatkit as pw
+
+ txt = """Python is an interpreted high-level general-purpose programminh language"""
+
+ pw.text_to_handwriting(txt,"handwritting project.png",[0,0,138])
+ print ("END")
